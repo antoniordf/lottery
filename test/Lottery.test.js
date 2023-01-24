47e5cb96cd960bee3ac9ca6588b69cc4c4e64f83
Antonio Furtado <antoniordf@hotmail.com>
2023-01-24 14:40:06 +0000

    Implemented additional test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -91,13 +91,10 @@ describe('Lottery Contract', () => {
     });
 
     const initialBalance = await web3.eth.getBalance(accounts[0]);
-
     await lottery.methods.pickWinner().send({ from: accounts[0] });
-
     const finalBalance = await web3.eth.getBalance(accounts[0]);
-
     const difference = finalBalance - initialBalance;
-    console.log(difference);
+
     assert(difference > web3.utils.toWei('1.8', 'ether'));
   });
 });
